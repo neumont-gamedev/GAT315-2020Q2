c13cebfd00f45c5d8957482a938040fa1dc165da
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2020-05-08 00:50:54 -0600

    Forward Kinematics

---
+++ Assets/Scripts/Kinematics/ForwardKinematicSegment.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardKinematicSegment : MonoBehaviour
+{
+	[SerializeField] [Range(-90.0f, 90.0f)] float m_angle = 0.0f;
+	[SerializeField] bool m_enableNoise = false;
+
+	public ForwardKinematicSegment parent { get; set; }
+
+	public Vector2 start { get { return transform.position; } set { transform.position = value; } }
+	public Vector2 end { get; set; }
+
+	public float length { get { return m_polar.length; } set { m_polar.length = value; } }
+	public float angle { get { return m_polar.angle; } set { m_polar.angle = value; } }
+	public float width { get; set; }
+	public float noiseRate { get; set; } = 1.0f;
+
+	public float baseAngle { get; set; }
+
+	Coordinate.Polar m_polar;
+	float m_noise;
+
+	private void Start()
+	{
+		m_noise = Random.value * 100.0f;
+	}
+
+	private void Update()
+	{
+		transform.localScale = Vector3.one * width;
+		float localAngle = m_angle;
+
+		if (m_enableNoise)
+		{
+			m_noise = m_noise + (Time.deltaTime * noiseRate);
+			float t = Mathf.PerlinNoise(m_noise, 0.0f);
+			localAngle = Mathf.Lerp(-90.0f, 90.0f, t);
+		}
+
+		angle = (parent != null) ? (localAngle + parent.angle) : (localAngle + baseAngle);
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	public void Initialize(ForwardKinematicSegment parent, Vector2 position, float angle, float length, float width)
+	{
+		this.parent = parent;
+		this.width = width;
+
+		this.angle = angle;
+		this.length = length;
+
+		start = position;
+		baseAngle = angle;
+	}
+
+	public void CalculateEnd()
+	{
+		Vector2 v2 = Coordinate.PolarToCartesian(m_polar);
+		end = start + v2;
+	}
+}
+
