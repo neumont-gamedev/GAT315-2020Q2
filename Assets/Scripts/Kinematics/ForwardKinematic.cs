adad82a258e2022985a4bb28f2b75d2cb778133c
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2020-05-10 17:17:08 -0600

    forward kinematics - class 050820

---
+++ Assets/Scripts/Kinematics/ForwardKinematic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardKinematic : MonoBehaviour
+{
+	[SerializeField] ForwardKinematicSegment m_segment = null;
+	[SerializeField] [Range(1, 40)] int m_count = 5;
+	[SerializeField] [Range(0.0f, 10.0f)] float m_length = 2.0f;
+	[SerializeField] [Range(0.0f, 10.0f)] float m_width = 1.0f;
+	[SerializeField] [Range(0.0f, 10.0f)] float m_noiseRate = 1.0f;
+
+	List<ForwardKinematicSegment> segments { get; set; } = new List<ForwardKinematicSegment>();
+
+	void Awake()
+	{
+		Vector2 position = transform.position;
+		float angle = transform.eulerAngles.z;
+
+		// base -> end
+		ForwardKinematicSegment parent = null;
+		for (int i = 0; i < m_count; i++)
+		{
+			ForwardKinematicSegment segment = Instantiate(m_segment, transform);
+			segment.Initialize(parent, position, angle, m_length, m_width);
+			segments.Add(segment);
+
+			parent = segment;
+		}
+	}
+
+	private void Update()
+	{
+		float noiseRate = m_noiseRate;
+		int count = 1;
+		foreach (ForwardKinematicSegment segment in segments)
+		{
+			segment.length = m_length;
+			segment.width = m_width;
+			segment.noiseRate = noiseRate * ((float)count / (float)segments.Count);
+			count++;
+
+			if (segment.parent != null)
+			{
+				segment.start = segment.parent.end;
+			}
+			segment.CalculateEnd();
+		}
+	}
+}
---
+++ Assets/Scripts/Kinematics/ForwardKinematicSegment.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardKinematicSegment : MonoBehaviour
+{
+	[SerializeField] [Range(-90.0f, 90.0f)] float m_angle = 0.0f;
+	[SerializeField] bool m_enableNoise = false;
+
+	public ForwardKinematicSegment parent { get; set; }
+
+	public Vector2 start { get { return transform.position; } set { transform.position = value; } }
+	public Vector2 end { get; set; }
+
+	public float length { get { return m_polar.length; } set { m_polar.length = value; } }
+	public float angle { get { return m_polar.angle; } set { m_polar.angle = value; } }
+	public float width { get; set; }
+	public float baseAngle { get; set; }
+	public float noiseRate { get; set; } = 1.0f;
+
+	Coordinate.Polar m_polar;
+	float m_noise;
+
+	private void Start()
+	{
+		m_noise = Random.value * 100.0f;
+	}
+
+	private void Update()
+	{
+		transform.localScale = Vector3.one * width;
+		float localAngle = m_angle;
+
+		if (m_enableNoise)
+		{
+			m_noise = m_noise + (noiseRate * Time.deltaTime);
+			float t = Mathf.PerlinNoise(m_noise, 0);
+			localAngle = Mathf.Lerp(-90, 90, t);
+		}
+
+		angle = (parent != null) ? (localAngle + parent.angle) : (localAngle + baseAngle);
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	public void Initialize(ForwardKinematicSegment parent, Vector2 position, float angle, float length, float width)
+	{
+		this.parent = parent;
+		this.width = width;
+
+		this.angle = angle;
+		this.length = length;
+
+		start = position;
+		baseAngle = angle;
+	}
+
+	public void CalculateEnd()
+	{
+		Vector2 offset = Coordinate.PolarToCartesian(m_polar);
+		end = start + offset;
+	}
+}
+
